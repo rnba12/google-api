02277d08436ed94ccca94545b568736f47e7b9a2
rnba12 <rnba12@yahoo.co.uk>
2022-11-16 18:58:22 +0000

    added stuff

--- client/script.js
+++ client/script.js
@@ -1,5 +1,5 @@
-const search = document.getElementById("search-btn")
-search.addEventListener("click",getResults)
+const search = document.querySelector("form")
+//search.addEventListener("click",getResults)
 
 
 function getResults(e){
@@ -14,15 +14,31 @@ function getResults(e){
             return true
         } return false;
     })
-    console.log(searchClothes)
     if (searchClothes) {
-        document.getElementById("one_displayURL").textContent ="";
+       searchi()
     }
 }
-const myForm = document.querySelector('form')
 
-myForm.addEventListener('submit', search)
 
-function search(e) {
+search.addEventListener('submit', getResults)
+
+function searchi(e) {
+    // e.preventDefault()
+    let url = `http://localhost:3000/google/clothes`;
+    fetch(url)
+    .then(r => r.json())
+    .then(Data => {
+        for (let i = 1; i < 11; i++) 
+    {
+        document.getElementById(`${i}_displayURL`).textContent = Data[`result${i}`].displayurl
+        document.getElementById(`${i}_heading`).textContent = Data[`result${i}`].heading
+        document.getElementById(`${i}_heading`).setAttribute('href', Data[`result${i}`].landingpage)
+        document.getElementById(`${i}_description`).textContent = Data[`result${i}`].description
+        // document.getElementById(`${i}_extensions1`).textContent = Data[`result${i}`].extentions.extentionsOne
+        // document.getElementById(`${i}_extensions2`).textContent = Data[`result${i}`].extentions.extentionsTwo
+        // document.getElementById(`${i}_extensions3`).textContent = Data[`result${i}`].extentions.extentionsThree
+        // document.getElementById(`${i}_extensions4`).textContent = Data[`result${i}`].extentions.extentionsFour
+    }
+    })
     
 }
