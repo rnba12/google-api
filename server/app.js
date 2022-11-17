85bd976d182be8d7451675f5577ec11ca9ab4afc
“shedp” <sheenadiep@gmail.com>
2022-11-17 14:05:36 +0000

    something happed...

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const routes = require('./routes')
+const routes = require('./routes').default
 app.use(express.json())
 const port = 3000
 
